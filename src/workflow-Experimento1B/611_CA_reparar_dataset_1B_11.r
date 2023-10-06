30a8decad6bc9dc45602047b0cc97235f1f71176
GermanLeonarhdt <gleonarhdt@itba.edu.ar>
2023-10-06 18:15:10 -0300

    ACtualizo 11

--- src/workflow-Experimento1B/611_CA_reparar_dataset_1B_11.r
+++ src/workflow-Experimento1B/611_CA_reparar_dataset_1B_11.r
@@ -10,7 +10,7 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "CA6110_1B_02"
+PARAM$experimento <- "CA6110_1B_11"
 PARAM$dataset <- "./datasets/competencia_2023.csv.gz"
 
 # valores posibles
