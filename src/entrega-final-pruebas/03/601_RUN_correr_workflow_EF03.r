75e05be7bf2e3a56ffd9ae635d6d504cf770688e
GermanLeonarhdt <gleonarhdt@itba.edu.ar>
2023-10-10 15:50:37 +0000

    EF03

---
+++ src/entrega-final-pruebas/03/601_RUN_correr_workflow_EF03.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/dm2023b/src/entrega-final-pruebas/01/611_CA_reparar_dataset_EF03.r")
+source("~/dm2023b/src/entrega-final-pruebas/01/621_DR_corregir_drifting_EF03.r")
+source("~/dm2023b/src/entrega-final-pruebas/01/631_FE_historia_EF03.r")
+source("~/dm2023b/src/entrega-final-pruebas/01/641_TS_training_strategy_EF03.r")
+
+# ultimos pasos, muy lentos
+source("~/dm2023b/src/entrega-final-pruebas/01/651_HT_lightgbm_EF03.r")
+source("~/dm2023b/src/entrega-final-pruebas/01/661_ZZ_final_EF03.r")
